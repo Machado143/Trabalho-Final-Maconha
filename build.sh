9ea90d96870189469b5d29623eab9322c2feb339
Machado143 <machadogm14@gmail.com>
2025-12-02 13:38:56 -0300

    superuser

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Cria superuser se não existir
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', '1234')" | python manage.py shell
